58486aac42d20b91004e469bee1618caad373046
simonmeredith <simonmeredith1@gmail.com>
2023-06-07 13:59:11 +0100

    Search valuesets (#1)
    * add valueset search initial commit
    
    * add valueset search working version
    
    * removed dependency from pom

--- src/main/java/TS.java
+++ src/main/java/TS.java
@@ -3,18 +3,42 @@ import com.facebook.presto.jdbc.internal.okhttp3.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
+import org.json.JSONObject;
 
 public class TS {
 
     public static void main(String[] args) {
-GetToken();
+ValueSetSearch();
     }
 
-    public static void GetToken() {
+    public static String GetToken() {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://ontology.scot.nhs.uk/authorisation/auth/realms/terminology/protocol/openid-connect/token"))
                 .header("Content-Type","application/x-www-form-urlencoded")
-                .method("POST",HttpRequest.BodyPublishers.ofString("grant_type=client_credentials&client_id=connectathon-consumer&client_secret=xxx"))
+                .method("POST",HttpRequest.BodyPublishers.ofString("grant_type=client_credentials&client_id=connectathon-consumer&client_secret=client_secret"))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request,HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert response != null;
+
+        String jsonBody = response.body();
+        JSONObject jsonObject = new JSONObject(jsonBody);
+        String access_token = jsonObject.getString("access_token");
+
+        return access_token;
+    }
+
+    public static void ValueSetSearch() {
+        String token = GetToken();
+        String x = "v";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ontology.scot.nhs.uk/production1/fhir/ValueSet?name=phys"))
+                .header("Authorization",token)
+                .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = null;
         try {
@@ -25,7 +49,6 @@ GetToken();
         assert response != null;
         System.out.println(response.body());
     }
-
 
     public static void PostmanCollectionCode() throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
