6ba3f2005cd81183cfd40f90fc525bbbc83998c0
User <User@Surface.tpmde.ac.uk>
2023-06-07 12:08:07 +0100

    initial commit

---
+++ src/main/java/TS.java
@@ -0,0 +1,41 @@
+import com.facebook.presto.jdbc.internal.okhttp3.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+
+public class TS {
+
+    public static void main(String[] args) {
+GetToken();
+    }
+
+    public static void GetToken() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ontology.scot.nhs.uk/authorisation/auth/realms/terminology/protocol/openid-connect/token"))
+                .header("Content-Type","application/x-www-form-urlencoded")
+                .method("POST",HttpRequest.BodyPublishers.ofString("grant_type=client_credentials&client_id=connectathon-consumer&client_secret=xxx"))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request,HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert response != null;
+        System.out.println(response.body());
+    }
+
+
+    public static void PostmanCollectionCode() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id={{client_id}}&client_secret={{client_secret}}");
+        Request request = new Request.Builder()
+                .url("https://ontology.scot.nhs.uk/authorisation/auth/realms/terminology/protocol/openid-connect/token")
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+    }
+}
